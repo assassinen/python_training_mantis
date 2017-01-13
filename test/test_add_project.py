c5271207c42516154a67476522c4c24e78a32217
assassinen <assassinen@ya.ru>
2017-01-13 16:28:17 +0300

    Задание №25: Реализовать тесты для добавления и удаления проектов в Mantis

---
+++ test/test_add_project.py
@@ -0,0 +1,14 @@
+__author__ = 'NovikovII'
+
+from model.project import Project
+import random
+
+def test_add_project(app):
+    app.session.ensure_login(username=app.config['webadmin']['username'], password=app.config['webadmin']['password'])
+    old_projects = app.project.get_project_list()
+    project = Project(name="New Project ({0})".format(random.randrange(1, 999999)),
+                      status="development", view_state="public", description="New Project Description", active=True)
+    app.project.create(project)
+    new_projects = app.project.get_project_list()
+    old_projects.append(project)
+    assert sorted(old_projects, key=Project.id_or_max) == sorted(new_projects, key=Project.id_or_max)
---
+++ test/test_delete_project.py
@@ -0,0 +1,17 @@
+__author__ = 'NovikovII'
+
+from model.project import Project
+import random
+
+def test_delete_some_project(app):
+    app.session.ensure_login(username=app.config['webadmin']['username'], password=app.config['webadmin']['password'])
+    if len(app.project.get_project_list()) == 0:
+        app.project.create(Project(name="Some Project ({0})".format(random.randrange(1, 999999)), status="release",
+                                   view_state="private", description="Some Project Description", active=True))
+    old_projects = app.project.get_project_list()
+    project = random.choice(old_projects)
+    app.project.delete_project_by_id(project.id)
+    new_projects = app.project.get_project_list()
+    assert len(old_projects) - 1 == len(new_projects)
+    old_projects.remove(project)
+    assert old_projects == new_projects
